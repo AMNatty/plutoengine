a7c634075193c708761832c8e5c9aab3421aa648
Natty <31473117+493msi@users.noreply.github.com>
2022-04-18 12:54:28 +0200

    Version bump

--- buildSrc/src/main/kotlin/org/plutoengine/Versions.kt
+++ buildSrc/src/main/kotlin/org/plutoengine/Versions.kt
@@ -23,7 +23,7 @@ object Versions {
 
     const val isPrerelease = true
     const val prereleaseName = "alpha"
-    const val prerealeaseUpdate = 0
+    const val prerealeaseUpdate = 1
 
     val versionFull =
         if (isPrerelease)
